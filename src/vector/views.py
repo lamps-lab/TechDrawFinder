42837de5a113890d74419dac98beeccd7d51fb28
choudhurym <muntabirc@gmail.com>
2023-10-03 01:25:11 -0400

    updated backend views.py and front-end searched.html files

--- src/vector/views.py
+++ src/vector/views.py
@@ -76,6 +76,10 @@ def read_img_path(file):
 
 ## This is a search function which will take user query and returns similar result
 
+index = faiss.read_index('./../vector_search/vector/vector_db/large_v1.index')
+fn = pd.read_csv('./../vector_search/patent_67000_segmented_metadata.csv')
+caps = fn['object_infer']
+viewpoint = fn['aspect']
 def searched(request):
 	
 	if request.method == "POST" and request.FILES:
@@ -83,8 +87,6 @@ def searched(request):
 		fs = FileSystemStorage() ##user query will be saved
 		files = request.FILES.get('file')
 		filename = fs.save(files.name, files)
-
-		index = faiss.read_index('/Users/muntabir/search-engine/vector_search/vector/vector_db/image.index')
 		
 		path = '/Users/muntabir/search-engine/vector_search/media/'
 		
@@ -93,38 +95,41 @@ def searched(request):
 
 
 		query_image = Image.open((os.path.join(path)+ filename), 'r')
+		query_image = query_image.resize((200, 200))
 		
 		query_embedding = get_single_image_embedding(query_image)
 		
 		query = np.array(query_embedding)
-		D, I = index.search(query, k=7)
-		fn = pd.read_csv('./../vector_search/imageonly.csv')
+		D, I = index.search(query, k=9)
+		
 		images = []
 		for x in I:
 			for pic in x:
-				result = open(fn['img_only'][pic], 'r')				
-				images.append(result.name.split('/')[-1])
-				
+				result = open(fn['figure_path'][pic], 'r')
+				img_captions = (caps[pic] + ", viewpoint: "+ viewpoint[pic])				
+				images.append((result.name.split('/')[-1], img_captions))			
+
 		return render(request, 'searched.html', {'query':url_img, 'img':images})
 
 	else:
 		txt = request.GET.get("query")
 		
-		txt_idx = faiss.read_index('/Users/muntabir/search-engine/vector_search/vector/vector_db/text.index')
+		#txt_idx = faiss.read_index('/Users/muntabir/search-engine/vector_search/vector/vector_db/text.index')
 		
 		if txt is not None:
 			text_query = get_single_text_embedding(txt)
 		
 			txtQuery = np.array(text_query)
 		
-			D, I = txt_idx.search(txtQuery, k = 7)
+			D, I = index.search(txtQuery, k = 9)
 		
 			texts = []
-			fn = pd.read_csv('./../vector_search/imageonly.csv')
+			#fn = pd.read_csv('./../vector_search/patent_67000_segmented_metadata.csv')
 			for x in I:
 				for txt in x:
-					res = open(fn['img_only'][txt], 'r')
-					texts.append(res.name.split('/')[-1])
+					res = open(fn['figure_path'][txt], 'r')
+					txt_captions = (caps[txt] + ", viewpoint: "+ viewpoint[txt])
+					texts.append((res.name.split('/')[-1], txt_captions))
 
 			return render(request, 'searched.html', {'img':texts})
 
